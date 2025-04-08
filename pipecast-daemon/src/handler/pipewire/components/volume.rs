399b582561961d1683a41a2500c72e675cb10f0e
Craig McLure <craig@mclure.net>
2025-04-08 20:40:10 +0100

    Added volume_load, to apply volumes from the profile

--- pipecast-daemon/src/handler/pipewire/components/volume.rs
+++ pipecast-daemon/src/handler/pipewire/components/volume.rs
@@ -8,12 +8,31 @@ use pipecast_shared::{Mix, MuteState, NodeType};
 use ulid::Ulid;
 
 pub(crate) trait VolumeManager {
+    async fn volumes_load(&self) -> Result<()>;
+
     async fn set_source_node_volume(&mut self, id: Ulid, mix: Mix, volume: u8) -> Result<()>;
     async fn set_target_node_volume(&mut self, id: Ulid, volume: u8) -> Result<()>;
     fn get_node_volume(&self, id: Ulid, mix: Mix) -> Result<u8>;
 }
 
 impl VolumeManager for PipewireManager {
+    async fn volumes_load(&self) -> Result<()> {
+        // Need to go through the various node types, and call a volume set
+        for device in &self.profile.devices.sources.physical_devices {
+            self.volume_source_load_with_mute(device.description.id).await?;
+        }
+        for device in &self.profile.devices.sources.virtual_devices {
+            self.volume_source_load_with_mute(device.description.id).await?;
+        }
+        for device in &self.profile.devices.targets.virtual_devices {
+            self.volume_target_load_with_mute(device.description.id, device.volume).await?;
+        }
+        for device in &self.profile.devices.targets.physical_devices {
+            self.volume_target_load_with_mute(device.description.id, device.volume).await?;
+        }
+        Ok(())
+    }
+
     async fn set_source_node_volume(&mut self, id: Ulid, mix: Mix, volume: u8) -> Result<()> {
         if !(0..=100).contains(&volume) {
             bail!("Volume Must be between 0 and 100");
@@ -92,6 +111,56 @@ impl VolumeManager for PipewireManager {
             NodeType::VirtualTarget => {
                 Ok(self.get_virtual_target(id).ok_or(err)?.volume)
             }
+        }
+    }
+}
+
+trait VolumeManagerLocal {
+    async fn volume_source_load_with_mute(&self, id: Ulid) -> Result<()>;
+    async fn volume_target_load_with_mute(&self, id: Ulid, volume: u8) -> Result<()>;
+}
+
+impl VolumeManagerLocal for PipewireManager {
+    async fn volume_source_load_with_mute(&self, id: Ulid) -> Result<()> {
+        let err = anyhow!("Unable to Locate Node");
+        let node_type = self.get_node_type(id).ok_or(err)?;
+        if !matches!(node_type, NodeType::PhysicalSource | NodeType::VirtualSource) {
+            bail!("Provided Source is a Target Node");
+        }
+
+        let err = anyhow!("Unable to Locate Mixes for Node");
+        let mixes = self.source_map.get(&id).ok_or(err)?;
+
+        let (a, b) = if self.is_source_muted_to_all(id).await? {
+            (0, 0)
+        } else {
+            let err = anyhow!("Unable to Find Node");
+            let volumes = if node_type == NodeType::PhysicalSource {
+                &self.get_physical_source(id).ok_or(err)?.volumes
+            } else {
+                &self.get_virtual_source(id).ok_or(err)?.volumes
+            };
+            (volumes.volume[Mix::A], volumes.volume[Mix::B])
+        };
+
+        self.filter_volume_set(mixes[Mix::A], a).await?;
+        self.filter_volume_set(mixes[Mix::B], b).await?;
+
+        Ok(())
+    }
+
+    async fn volume_target_load_with_mute(&self, id: Ulid, volume: u8) -> Result<()> {
+        let err = anyhow!("Unable to Locate Node");
+        let node_type = self.get_node_type(id).ok_or(err)?;
+        if !matches!(node_type, NodeType::PhysicalTarget | NodeType::VirtualTarget) {
+            bail!("Provided Source is a Source Node");
+        }
+
+        let target = self.get_target_filter_node(id)?;
+        if self.get_target_mute_state(id).await? == MuteState::Muted {
+            self.filter_volume_set(target, 0).await
+        } else {
+            self.filter_volume_set(target, volume).await
         }
     }
 }
