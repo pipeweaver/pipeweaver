41fdd92c952e2da85ad32f4fe42f0bb92d04daca
Craig McLure <craig@mclure.net>
2025-04-08 19:13:12 +0100

    Add function to load the routing table

--- pipecast-daemon/src/handler/pipewire/components/routing.rs
+++ pipecast-daemon/src/handler/pipewire/components/routing.rs
@@ -8,11 +8,34 @@ use pipecast_shared::{Mix, NodeType};
 use ulid::Ulid;
 
 pub(crate) trait RoutingManagement {
+    async fn load_routing(&mut self) -> Result<()>;
+
     async fn routing_set_route(&mut self, source: Ulid, target: Ulid, enabled: bool) -> Result<()>;
     async fn routing_route_exists(&self, source: Ulid, target: Ulid) -> Result<bool>;
 }
 
 impl RoutingManagement for PipewireManager {
+    async fn load_routing(&mut self) -> Result<()> {
+        // This should be called after all the nodes are set up, we need to check our routing table
+        // and establish links between the sources and targets
+
+        let routing = &self.profile.routes;
+        for (source, targets) in routing {
+            for target in targets {
+                let target = self.get_target_node(*target)?;
+                if !self.is_source_muted_to_some(*source, target).await? {
+                    if let Some(map) = self.source_map.get(source).copied() {
+                        self.link_create_filter_to_filter(map[Mix::A], target).await?;
+                        self.link_create_filter_to_filter(map[Mix::B], target).await?;
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+
     async fn routing_set_route(&mut self, source: Ulid, target: Ulid, enabled: bool) -> Result<()> {
         // This is actually more complicated that it sounds, first lets find some stuff out..
         let source_type = self.get_node_type(source).ok_or(anyhow!("Source Not Found"))?;
@@ -26,14 +49,8 @@ impl RoutingManagement for PipewireManager {
             bail!("Provided Target is a Source Node");
         }
 
-        let error = "[INTERNAL] Broken Target Map";
-        let target_id = match target_type {
-            NodeType::PhysicalTarget => target,
-            NodeType::VirtualTarget => *self.target_map.get(&target).ok_or(anyhow!(error))?,
-            _ => bail!("Target is a Source!")
-        };
-
         // This should already be here, but it's not, so create it
+        let target_id = self.get_target_node(target)?;
         if self.profile.routes.get(&source).is_none() {
             warn!("[Routing] Table Missing for Source {}, Creating", source);
             self.profile.routes.insert(source, Default::default());
