3d08dbd8bb56522eb22bf580d11ed0f63b5ec3ed
Craig McLure <craig@mclure.net>
2025-12-03 05:47:19 +0000

    Support generating .ico files

--- daemon/resources/icons/generate_icons.sh
+++ daemon/resources/icons/generate_icons.sh
@@ -6,3 +6,14 @@
 
 inkscape pipeweaver.svg --export-filename=pipeweaver-large.png -w 128 -h 128
 inkscape pipeweaver.svg --export-filename=pipeweaver.png -w 48 -h 48
+
+## Note, this requires imagemagick to be installed to create the various Windows icon sizes from SVG..
+mkdir tmp/
+FILE_PATHS=""
+for size in 16 32 48 128 256; do
+  FILE_NAME="tmp/$size.png"
+  inkscape pipeweaver.svg --export-filename=$FILE_NAME -w $size -h $size
+  FILE_PATHS="${FILE_PATHS} ${FILE_NAME}"
+done
+magick $FILE_PATHS goxlr-utility.ico
+rm -r tmp/
