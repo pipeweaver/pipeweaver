1535dac9148de65076c24b9128d39c46ca5cd058
Craig McLure <craig@mclure.net>
2026-03-22 04:50:37 +0000

    Initial client pass, not working yet :D

--- client/src/cli.rs
+++ client/src/cli.rs
@@ -1,6 +1,193 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use pipeweaver_shared::{
+    Colour, DeviceType, Mix, MuteState, MuteTarget, NodeType, OrderGroup, Quantum,
+};
 
+/// PipeWeaver CLI
 #[derive(Parser, Debug)]
 #[command(about, version, author)]
 #[command(arg_required_else_help = true)]
-pub struct Cli {}
+pub struct Cli {
+    /// Display status after command.
+    #[arg(long)]
+    pub status: bool,
+
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+#[command(arg_required_else_help = true)]
+pub enum Commands {
+    /// Node-related commands (by node ID or for creation)
+    Node {
+        #[command(subcommand)]
+        command: NodeCommands,
+    },
+    /// Route-related commands
+    Route {
+        #[command(subcommand)]
+        command: RouteCommands,
+    },
+    /// Application-related commands
+    App {
+        #[command(subcommand)]
+        command: AppCommands,
+    },
+    /// Daemon/Device-related commands
+    Daemon {
+        #[command(subcommand)]
+        command: DaemonCommands,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+#[command(arg_required_else_help = true)]
+pub enum NodeCommands {
+    /// Create a new node
+    Create {
+        #[arg(value_enum)]
+        node_type: NodeType,
+        name: String,
+    },
+    /// Operate on an existing node by ID
+    Edit {
+        name: String,
+        #[command(subcommand)]
+        command: NodeIdCommands,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+#[command(arg_required_else_help = true)]
+pub enum NodeIdCommands {
+    Rename {
+        name: String,
+    },
+    SetColour {
+        colour: Colour,
+    },
+    Remove,
+    SetVolume {
+        /// Volume as a percentage (0-100)
+        #[arg(value_parser = percent_value)]
+        volume: u8,
+
+        /// The Mix to be adjusted
+        mix: Option<Mix>,
+    },
+    SetSourceVolumeLinked {
+        linked: bool,
+    },
+    SetTargetMix {
+        #[arg(value_enum)]
+        mix: Mix,
+    },
+    AddSourceMuteTarget {
+        #[arg(value_enum)]
+        target: MuteTarget,
+    },
+    DelSourceMuteTarget {
+        #[arg(value_enum)]
+        target: MuteTarget,
+    },
+    AddMuteTargetNode {
+        #[arg(value_enum)]
+        target: MuteTarget,
+        node: String,
+    },
+    DelMuteTargetNode {
+        #[arg(value_enum)]
+        target: MuteTarget,
+        node: String,
+    },
+    ClearMuteTargetNodes {
+        #[arg(value_enum)]
+        target: MuteTarget,
+    },
+    SetTargetMuteState {
+        #[arg(value_enum)]
+        state: MuteState,
+    },
+    AttachPhysicalNode {
+        device: u32,
+    },
+    RemovePhysicalNode {
+        index: usize,
+    },
+    SetOrderGroup {
+        #[arg(value_enum)]
+        group: OrderGroup,
+    },
+    SetOrder {
+        order: u8,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+#[command(arg_required_else_help = true)]
+pub enum RouteCommands {
+    Set {
+        source: String,
+        target: String,
+        enabled: bool,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+#[command(arg_required_else_help = true)]
+pub enum AppCommands {
+    SetRoute {
+        device_type: DeviceType,
+        process: String,
+        name: String,
+        target: String,
+    },
+    ClearRoute {
+        device_type: DeviceType,
+        process: String,
+        name: String,
+    },
+    SetTransientRoute {
+        process_id: u32,
+        target: String,
+    },
+    ClearTransientRoute {
+        process_id: u32,
+    },
+    SetVolume {
+        process_id: u32,
+        /// Volume as a percentage (0-100)
+        #[arg(value_parser = percent_value)]
+        volume: u8,
+    },
+    SetMute {
+        process_id: u32,
+        muted: bool,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+#[command(arg_required_else_help = true)]
+pub enum DaemonCommands {
+    SetAutoStart {
+        enabled: bool,
+    },
+    SetAudioQuantum {
+        #[arg(value_enum)]
+        quantum: Quantum,
+    },
+    OpenInterface,
+    ResetAudio,
+}
+
+// Example argument validation function (if needed)
+pub fn percent_value(s: &str) -> Result<u8, String> {
+    let value = s
+        .parse::<u8>()
+        .map_err(|_| "Value must be between 0 and 100".to_string())?;
+    if value > 100 {
+        return Err("Value must be lower than 100".to_string());
+    }
+    Ok(value)
+}
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,130 @@
 mod cli;
+use anyhow::Result;
+use clap::Parser;
+use pipeweaver_ipc::commands::{APICommand, DaemonCommand, DaemonRequest};
+use pipeweaver_shared::AppDefinition;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    let cli = cli::Cli::parse();
+
+    let msg = match cli.command {
+        Some(command) => match command {
+            cli::Commands::Node { command } => handle_node_command(command),
+            cli::Commands::App { command } => handle_app_command(command),
+            cli::Commands::Route { command } => handle_route_command(command),
+            cli::Commands::Daemon { command } => handle_daemon_command(command),
+        },
+        None => return Ok(()),
+    };
+    println!("PipeWeaver message: {msg:#?}");
+
+    Ok(())
+}
+
+fn handle_node_command(cmd: cli::NodeCommands) -> DaemonRequest {
+    use cli::NodeCommands::*;
+    use cli::NodeIdCommands as IdCmd;
+    let api_cmd = match cmd {
+        Create { node_type, name } => APICommand::CreateNode(node_type, name),
+        Edit {
+            name: src_name,
+            command,
+        } => match command {
+            IdCmd::Rename { name } => APICommand::RenameNodeByName(src_name, name),
+            IdCmd::SetColour { colour } => APICommand::SetNodeColourByName(src_name, colour),
+            IdCmd::Remove => APICommand::RemoveNodeByName(src_name),
+            IdCmd::SetVolume { mix, volume } => APICommand::SetVolumeByName(src_name, mix, volume),
+            IdCmd::SetSourceVolumeLinked { linked } => {
+                APICommand::SetSourceVolumeLinkedByName(src_name, linked)
+            }
+            IdCmd::SetTargetMix { mix } => APICommand::SetTargetMixByName(src_name, mix),
+            IdCmd::AddSourceMuteTarget { target } => {
+                APICommand::AddSourceMuteTargetByName(src_name, target)
+            }
+            IdCmd::DelSourceMuteTarget { target } => {
+                APICommand::DelSourceMuteTargetByName(src_name, target)
+            }
+            IdCmd::AddMuteTargetNode { target, node } => {
+                APICommand::AddMuteTargetNodeByNames(src_name, target, node)
+            }
+            IdCmd::DelMuteTargetNode { target, node } => {
+                APICommand::DelMuteTargetNodeByNames(src_name, target, node)
+            }
+            IdCmd::ClearMuteTargetNodes { target } => {
+                APICommand::ClearMuteTargetNodesByName(src_name, target)
+            }
+            IdCmd::SetTargetMuteState { state } => {
+                APICommand::SetTargetMuteStatesByName(src_name, state)
+            }
+            IdCmd::AttachPhysicalNode { device } => {
+                APICommand::AttachPhysicalNodeByName(src_name, device)
+            }
+            IdCmd::RemovePhysicalNode { index } => {
+                APICommand::RemovePhysicalNodeByName(src_name, index)
+            }
+            IdCmd::SetOrderGroup { group } => APICommand::SetOrderGroupByName(src_name, group),
+            IdCmd::SetOrder { order } => APICommand::SetOrderByName(src_name, order),
+        },
+    };
+    DaemonRequest::Pipewire(api_cmd)
+}
+
+fn handle_app_command(cmd: cli::AppCommands) -> DaemonRequest {
+    use cli::AppCommands::*;
+    let api_cmd = match cmd {
+        SetRoute {
+            device_type,
+            process,
+            name,
+            target,
+        } => APICommand::SetApplicationRouteByName(
+            AppDefinition {
+                device_type,
+                process,
+                name,
+            },
+            target,
+        ),
+        ClearRoute {
+            device_type,
+            process,
+            name,
+        } => APICommand::ClearApplicationRoute(AppDefinition {
+            device_type,
+            process,
+            name,
+        }),
+        SetTransientRoute { process_id, target } => {
+            APICommand::SetTransientApplicationRouteByName(process_id, target)
+        }
+        ClearTransientRoute { process_id } => {
+            APICommand::ClearTransientApplicationRoute(process_id)
+        }
+        SetVolume { process_id, volume } => APICommand::SetApplicationVolume(process_id, volume),
+        SetMute { process_id, muted } => APICommand::SetApplicationMute(process_id, muted),
+    };
+    DaemonRequest::Pipewire(api_cmd)
+}
+
+fn handle_route_command(cmd: cli::RouteCommands) -> DaemonRequest {
+    use cli::RouteCommands::*;
+    let api_cmd = match cmd {
+        Set {
+            source,
+            target,
+            enabled,
+        } => APICommand::SetRouteByNames(source, target, enabled),
+    };
+    DaemonRequest::Pipewire(api_cmd)
+}
+
+fn handle_daemon_command(cmd: cli::DaemonCommands) -> DaemonRequest {
+    use cli::DaemonCommands::*;
+    let daemon_cmd = match cmd {
+        SetAutoStart { enabled } => DaemonCommand::SetAutoStart(enabled),
+        SetAudioQuantum { quantum } => DaemonCommand::SetAudioQuantum(quantum),
+        OpenInterface => DaemonCommand::OpenInterface,
+        ResetAudio => DaemonCommand::ResetAudio,
+    };
+    DaemonRequest::Daemon(daemon_cmd)
 }
