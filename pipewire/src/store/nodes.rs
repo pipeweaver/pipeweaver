4ab6fb2c7bafac579300e9b2ffa6951dc38d2d99
Craig McLure <craig@mclure.net>
2026-08-23 15:03:25 +0100

    Moved node creation

--- pipewire/src/store/nodes.rs
+++ pipewire/src/store/nodes.rs
@@ -1,18 +1,30 @@
 //! Lifecycle of the nodes *we* create (as opposed to `unmanaged_devices`,
 //! which tracks nodes pipewire owns): registration, waiting for pipewire to
-//! assign an id and ports, state tracking, and volume/mute control for both
-//! our own nodes and unmanaged client (application) nodes.
+//! assign an id and ports, state tracking, and volume/mute control.
 
 use super::{ManagedNode, NodeStoreState, PortLocation, Store};
-use crate::PipewireReceiver;
+use crate::{MediaClass, NodeProperties, PipewireReceiver};
 use anyhow::{Result, anyhow};
 use log::{debug, error};
+use pipewire::core::CoreRc;
+use pipewire::keys::{
+    APP_ICON_NAME, AUDIO_CHANNELS, DEVICE_ICON_NAME, FACTORY_NAME, MEDIA_CLASS, MEDIA_ICON_NAME,
+    NODE_ALWAYS_PROCESS, NODE_DESCRIPTION, NODE_DRIVER, NODE_FORCE_QUANTUM, NODE_FORCE_RATE,
+    NODE_GROUP, NODE_NAME, NODE_NICK, NODE_VIRTUAL, OBJECT_LINGER, PORT_MONITOR,
+};
+use pipewire::node::NodeChangeMask;
+use pipewire::properties::properties;
+use pipewire::proxy::ProxyT;
 use pipewire::spa::param::ParamType;
-use pipewire::spa::pod::serialize::PodSerializer;
-use pipewire::spa::pod::{Pod, Property, Value, ValueArray, object};
-use pipewire::spa::sys::{SPA_PROP_channelVolumes, SPA_PROP_mute};
-use pipewire::spa::utils;
-use std::io::Cursor;
+use pipewire::spa::pod::deserialize::PodDeserializer;
+use pipewire::spa::pod::{Value, ValueArray};
+use pipewire::spa::sys::{
+    SPA_AUDIO_CHANNEL_FL, SPA_AUDIO_CHANNEL_FR, SPA_FORMAT_AUDIO_position,
+    SPA_PARAM_PORT_CONFIG_format, SPA_PARAM_PortConfig, SPA_PARAM_Props, SPA_PROP_channelVolumes,
+    SPA_PROP_mute,
+};
+use std::cell::RefCell;
+use std::rc::Weak;
 use ulid::Ulid;
 
 impl Store {
@@ -22,6 +34,255 @@ impl Store {
         self.managed_nodes
             .values()
             .any(|node| node.pw_id == Some(id))
+    }
+
+    pub fn create_node(
+        &mut self,
+        core: &CoreRc,
+        properties: NodeProperties,
+        listener_store: Weak<RefCell<Store>>,
+    ) -> Result<()> {
+        let node_properties = &mut properties! {
+            *FACTORY_NAME => "support.null-audio-sink",
+            *NODE_NAME => properties.node_name.clone(),
+            *NODE_NICK => properties.node_nick,
+            *NODE_DESCRIPTION => properties.node_description,
+
+            *NODE_ALWAYS_PROCESS => "true",
+            *NODE_VIRTUAL => "true",
+            *PORT_MONITOR => "false",
+
+            *APP_ICON_NAME => &*properties.app_id,
+            *MEDIA_ICON_NAME => &*properties.app_id,
+            *DEVICE_ICON_NAME => &*properties.app_id,
+
+            *NODE_GROUP => "pipeweaver-nodes",
+
+            //*APP_NAME => properties.app_name,
+            *OBJECT_LINGER => match properties.linger {
+                true => "true",
+                false => "false"
+            },
+            *MEDIA_CLASS => match properties.class {
+                MediaClass::Source => "Audio/Source/Virtual",
+                MediaClass::Duplex => "Audio/Duplex",
+                MediaClass::Sink => "Audio/Sink",
+            },
+
+            *AUDIO_CHANNELS => "2",
+
+            // Force the RATE to match the system rate
+            *NODE_FORCE_RATE => properties.rate.to_string(),
+
+            // We don't want to set a driver here. If creating a large number of nodes each of them
+            // will pick a different device while finding a clock source, resulting in the nodes
+            // being spread all over the place. When the node tree starts getting linked together
+            // pipewire needs to pull all the nodes / audio_filters / devices into a single clock source
+            // which can cause some pretty aggressive behaviours (I've seen it infinite loop as
+            // various nodes fight for clock control).
+            //
+            // Setting this to false means that the devices will fall under the 'Dummy' node until
+            // a physical device is attached, at which point it'll move everything together under
+            // that single clock.
+            *NODE_DRIVER => "false",
+
+            // https://gitlab.freedesktop.org/pipewire/pipewire/-/wikis/Virtual-Devices
+            "audio.position" => "FL,FR",
+
+            // If upstream is managing the volumes via a filter, we don't want Pipewire interfering
+            "monitor.channel-volumes" => match properties.managed_volume {
+                true => "false",
+                false => "true"
+            },
+        };
+
+        // If a quantum is provided, send it in to the props
+        if let Some(quantum) = properties.buffer {
+            node_properties.insert(*NODE_FORCE_QUANTUM, quantum.to_string());
+        }
+
+        debug!(
+            "[{}] Attempting to Create Device '{}'",
+            properties.node_id, properties.node_name
+        );
+
+        // Properties built, create the node.
+        let proxy = core
+            .create_object::<pipewire::node::Node>("adapter", node_properties)
+            .map_err(|e| anyhow!("Unable to Create Node {}", e))?;
+
+        // Set the Initial volume
+        super::utils::send_volume(&proxy, properties.initial_volume);
+
+        debug!("[{}] Registering Proxy Listener", properties.node_id);
+        let proxy_id = properties.node_id;
+        let proxy_store = listener_store.clone();
+        let proxy_listener = proxy
+            .upcast_ref()
+            .add_listener_local()
+            .bound(move |id| {
+                debug!("[{}] Pipewire NodeID assigned: {}", proxy_id, id);
+                if let Some(proxy_store) = proxy_store.upgrade() {
+                    proxy_store
+                        .borrow_mut()
+                        .managed_node_set_pw_id(proxy_id, id);
+                }
+            })
+            .removed(|| {
+                debug!("Removed..");
+            })
+            .register();
+
+        debug!("[{}] Registering Node Listener", properties.node_id);
+        let listener_id = properties.node_id;
+        let listener_info_store = listener_store.clone();
+        let listener_param_store = listener_store.clone();
+        let listener = proxy
+            .add_listener_local()
+            .info(move |info| {
+                // Check whether this is a PORT related message
+                if info.change_mask().contains(NodeChangeMask::INPUT_PORTS)
+                    || info.change_mask().contains(NodeChangeMask::OUTPUT_PORTS)
+                {
+                    // Now check whether our port count matches what's expected
+                    if info.n_input_ports() == 2 && info.n_output_ports() == 2 {
+                        debug!(
+                            "[{}] Ports have appeared, requesting configuration",
+                            listener_id
+                        );
+                        if let Some(store) = listener_info_store.upgrade() {
+                            store.borrow().managed_node_request_ports(listener_id);
+                        }
+                    }
+                }
+
+                if info.change_mask().contains(NodeChangeMask::STATE) {
+                    let new_state = NodeStoreState::from(info.state());
+
+                    if let Some(store) = listener_info_store.upgrade() {
+                        store
+                            .borrow_mut()
+                            .managed_node_state_changed(listener_id, new_state);
+                    }
+                }
+            })
+            .param(move |_seq, _type, _index, _next, param| {
+                if let Some(pod) = param {
+                    let pod = PodDeserializer::deserialize_any_from(pod.as_bytes()).map(|(_, v)| v);
+                    if let Ok(Value::Object(object)) = pod {
+                        if object.id == SPA_PARAM_PortConfig {
+                            debug!("[{}] Port configuration Received", listener_id);
+                            let prop = object
+                                .properties
+                                .iter()
+                                .find(|p| p.key == SPA_PARAM_PORT_CONFIG_format);
+
+                            // Format is optional
+                            if let Some(prop) = prop
+                                && let Value::Object(object) = &prop.value
+                            {
+                                // Value is of type SPA_TYPE_OBJECT_Format
+                                let prop = object
+                                    .properties
+                                    .iter()
+                                    .find(|p| p.key == SPA_FORMAT_AUDIO_position);
+
+                                if let Some(prop) = prop {
+                                    // Fucking hell, I hate how deep this is getting
+                                    if let Value::ValueArray(ValueArray::Id(array)) = &prop.value
+                                        && let Some(listener_param_store) =
+                                            listener_param_store.upgrade()
+                                    {
+                                        let mut store = listener_param_store.borrow_mut();
+                                        for (index, value) in array.iter().enumerate() {
+                                            let index = index as u32;
+                                            if value.0 == SPA_AUDIO_CHANNEL_FL {
+                                                store.managed_node_add_port(
+                                                    listener_id,
+                                                    PortLocation::Left,
+                                                    index,
+                                                );
+                                            }
+                                            if value.0 == SPA_AUDIO_CHANNEL_FR {
+                                                store.managed_node_add_port(
+                                                    listener_id,
+                                                    PortLocation::Right,
+                                                    index,
+                                                );
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        } else if object.id == SPA_PARAM_Props {
+                            let prop = object
+                                .properties
+                                .iter()
+                                .find(|p| p.key == SPA_PROP_channelVolumes);
+
+                            // Get the Left / Right value
+                            if let Some(prop) = prop
+                                && let Value::ValueArray(ValueArray::Float(value)) = &prop.value
+                            {
+                                // OK, so KDE and pwvucontrol use the highest value for their reference
+                                let max = value
+                                    .iter()
+                                    .copied()
+                                    .max_by(|a, b| a.partial_cmp(b).unwrap())
+                                    .unwrap();
+
+                                let volume = (max.cbrt() * 100.0).round() as u8;
+                                if let Some(listener_param_store) = listener_param_store.upgrade() {
+                                    listener_param_store
+                                        .borrow_mut()
+                                        .on_volume_change(listener_id, volume);
+                                }
+                            }
+
+                            let prop = object.properties.iter().find(|p| p.key == SPA_PROP_mute);
+
+                            if let Some(prop) = prop
+                                && let Value::Bool(enabled) = &prop.value
+                                && let Some(listener_param_store) = listener_param_store.upgrade()
+                            {
+                                listener_param_store
+                                    .borrow_mut()
+                                    .on_mute_change(listener_id, *enabled);
+                            }
+                        } else {
+                            error!("Parameter Parse Error, Message was not of expected type");
+                            debug!("Object Id: {}", object.id);
+                            for property in object.properties {
+                                debug!("Key: {}, Value: {:?}", property.key, property.value);
+                            }
+                        }
+                    } else {
+                        error!("Unexpected Value Type");
+                    }
+                }
+            })
+            .register();
+        proxy.subscribe_params(&[ParamType::Props]);
+
+        let node = ManagedNode {
+            pw_id: None,
+            object_serial: None,
+            id: properties.node_id,
+            props: node_properties.clone(),
+            proxy,
+            _listener: listener,
+            _proxy_listener: proxy_listener,
+
+            port_map: Default::default(),
+            ports_ready: false,
+
+            node_state: NodeStoreState::Creating,
+            ready_sender: Some(properties.ready_sender),
+        };
+
+        self.managed_node_add(node);
+
+        Ok(())
     }
 
     pub fn managed_node_add(&mut self, node: ManagedNode) {
@@ -142,41 +403,7 @@ impl Store {
             .get(&id)
             .ok_or(anyhow!("Failed to find node"))?;
 
-        let volume = (volume as f32 / 100.0).powi(3);
-        let pod = Value::Object(object! {
-            utils::SpaTypes::ObjectParamProps,
-            ParamType::Props,
-            Property::new(SPA_PROP_channelVolumes, Value::ValueArray(ValueArray::Float(vec![volume, volume]))),
-        });
-
-        let (cursor, _) = PodSerializer::serialize(Cursor::new(Vec::new()), &pod).unwrap();
-        let bytes = cursor.into_inner();
-        if let Some(bytes) = Pod::from_bytes(&bytes) {
-            node.proxy.set_param(ParamType::Props, 0, bytes);
-        }
-        Ok(())
-    }
-
-    pub fn set_application_volume(&mut self, id: u32, volume: u8) -> Result<()> {
-        let node = self
-            .unmanaged_client_nodes
-            .get(&id)
-            .ok_or(anyhow!("Failed to find node"))?;
-
-        let volume = (volume as f32 / 100.0).powi(3);
-        let pod = Value::Object(object! {
-            utils::SpaTypes::ObjectParamProps,
-            ParamType::Props,
-            Property::new(SPA_PROP_channelVolumes, Value::ValueArray(ValueArray::Float(vec![volume, volume]))),
-        });
-
-        let (cursor, _) = PodSerializer::serialize(Cursor::new(Vec::new()), &pod).unwrap();
-        let bytes = cursor.into_inner();
-        if let Some(bytes) = Pod::from_bytes(&bytes)
-            && let Some(proxy) = &node.proxy
-        {
-            proxy.set_param(ParamType::Props, 0, bytes);
-        }
+        super::utils::send_volume(&node.proxy, volume);
         Ok(())
     }
 
@@ -192,38 +419,7 @@ impl Store {
             .get(&id)
             .ok_or(anyhow!("Failed to find node"))?;
 
-        let pod = Value::Object(object! {
-            utils::SpaTypes::ObjectParamProps,
-            ParamType::Props,
-            Property::new(SPA_PROP_mute, Value::Bool(muted)),
-        });
-        let (cursor, _) = PodSerializer::serialize(Cursor::new(Vec::new()), &pod)?;
-        let bytes = cursor.into_inner();
-        if let Some(bytes) = Pod::from_bytes(&bytes) {
-            node.proxy.set_param(ParamType::Props, 0, bytes);
-        }
-        Ok(())
-    }
-
-    pub fn set_application_muted(&mut self, id: u32, muted: bool) -> Result<()> {
-        let node = self
-            .unmanaged_client_node_get(id)
-            .ok_or(anyhow!("Failed to find node"))?;
-        let pod = Value::Object(object! {
-            utils::SpaTypes::ObjectParamProps,
-            ParamType::Props,
-            Property::new(SPA_PROP_mute, Value::Bool(muted)),
-        });
-        let (cursor, _) = PodSerializer::serialize(Cursor::new(Vec::new()), &pod).unwrap();
-        let bytes = cursor.into_inner();
-
-        // Create the POD and send it to the proxy
-        if let Some(bytes) = Pod::from_bytes(&bytes)
-            && let Some(proxy) = &node.proxy
-        {
-            proxy.set_param(ParamType::Props, 0, bytes);
-        }
-
+        super::utils::send_mute(&node.proxy, muted);
         Ok(())
     }
 
