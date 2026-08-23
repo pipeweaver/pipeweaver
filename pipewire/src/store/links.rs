b28863e66c4174dd358f4d9977bc2550b62b1433
Craig McLure <craig@mclure.net>
2026-08-23 15:17:00 +0100

    Filter and Link creation are managed by their associated store part

--- pipewire/src/store/links.rs
+++ pipewire/src/store/links.rs
@@ -5,9 +5,23 @@
 //! `pending_link_syncs` until that's true.
 
 use super::{ManagedLink, ManagedLinkMap, PendingLinkSync, PortLocation, Store};
-use crate::{LinkType, PipewireReceiver};
+use crate::{Direction, LinkType, PipewireReceiver};
+use anyhow::{Result, anyhow, bail};
 use log::{debug, error, warn};
+use oneshot::Sender;
+use pipewire::core::CoreRc;
+use pipewire::keys::{
+    LINK_INPUT_NODE, LINK_INPUT_PORT, LINK_OUTPUT_NODE, LINK_OUTPUT_PORT, NODE_PASSIVE,
+    OBJECT_LINGER,
+};
+use pipewire::link::{Link, LinkChangeMask, LinkState};
+use pipewire::properties::properties;
+use pipewire::proxy::ProxyT;
+use pipewire::registry::RegistryRc;
+use std::cell::{Cell, RefCell};
 use std::collections::HashMap;
+use std::rc::Weak;
+use std::str::FromStr;
 use strum::IntoEnumIterator;
 use ulid::Ulid;
 
@@ -25,6 +39,192 @@ impl Store {
             })
             .map(|(id, _)| id)
             .copied()
+    }
+
+    pub fn create_link(
+        &mut self,
+        core: &CoreRc,
+        registry: &RegistryRc,
+        source: LinkType,
+        dest: LinkType,
+        sender: Sender<()>,
+        listener_store: Weak<RefCell<Store>>,
+    ) -> Result<()> {
+        // Fetch the details of the links that need creating
+        let mut group = self.prepare_links(source, dest, sender, registry)?;
+
+        // Create a Parent ID for this link set
+        let parent_id = Ulid::generate();
+
+        // Find the first portmap and begin creation
+        for port in PortLocation::iter() {
+            let entry = &mut group.links[port];
+
+            if let Some(m) = entry.as_mut() {
+                // Ok, we have a LinkStoreMap, trigger the first creation.
+                create_port_link(core, parent_id, m, listener_store.clone())?;
+                break;
+            }
+        }
+        self.add_pending_link(parent_id, group);
+        Ok(())
+    }
+
+    pub fn prepare_links(
+        &mut self,
+        source: LinkType,
+        dest: LinkType,
+        sender: Sender<()>,
+        registry: &RegistryRc,
+    ) -> Result<ManagedLink> {
+        // First, check if a managed link already exists and remove it
+        self.managed_link_remove(&source, &dest);
+        let mut port_map: enum_map::EnumMap<PortLocation, Option<ManagedLinkMap>> =
+            Default::default();
+
+        // Collect all the port pairs we're going to link
+        let mut port_pairs = Vec::new();
+        for port in PortLocation::iter() {
+            let (_, src_index) = self.get_port(&source, Direction::Out, port)?;
+            let (_, tgt_index) = self.get_port(&dest, Direction::In, port)?;
+            port_pairs.push((src_index, tgt_index));
+        }
+
+        // Find and destroy any unmanaged links between these exact ports
+        let links_to_destroy: Vec<u32> = self
+            .get_unmanaged_links()
+            .iter()
+            .filter_map(|(id, link)| {
+                let should_remove = port_pairs.iter().any(|(out_port, in_port)| {
+                    link.output_port == *out_port && link.input_port == *in_port
+                });
+                if should_remove { Some(*id) } else { None }
+            })
+            .collect();
+
+        if !links_to_destroy.is_empty() {
+            debug!(
+                "Destroying {} orphaned unmanaged links in PipeWire: {:?}",
+                links_to_destroy.len(),
+                links_to_destroy
+            );
+            for link_id in links_to_destroy {
+                registry.destroy_global(link_id);
+                self.unmanaged_link_remove(link_id);
+            }
+        }
+
+        // Now create the links
+        for port in PortLocation::iter() {
+            // Firstly, create an id for this list
+            let link_id = Ulid::generate();
+
+            // Next, obtain the source and destination port indexes
+            let (src_id, src_index) = self.get_port(&source, Direction::Out, port)?;
+            let (tgt_id, tgt_index) = self.get_port(&dest, Direction::In, port)?;
+
+            // Create the LinkStore Mapping for this link
+            let store = ManagedLinkMap {
+                pw_id: None,
+                internal_id: link_id,
+
+                pending_seq_id: None,
+                _link: None,
+                _proxy_listener: None,
+                _info_listener: None,
+
+                source_port: (src_id, src_index),
+                destination_port: (tgt_id, tgt_index),
+            };
+
+            port_map[port] = Some(store);
+        }
+
+        // Ok, we're done here, create the main store object
+        let group = ManagedLink {
+            source: source.clone(),
+            destination: dest.clone(),
+            links: port_map,
+            ready_sender: Some(sender),
+        };
+
+        Ok(group)
+    }
+
+    pub fn remove_link(&mut self, source: LinkType, destination: LinkType) -> Result<()> {
+        self.managed_link_remove(&source, &destination);
+        Ok(())
+    }
+
+    pub(crate) fn get_port(
+        &mut self,
+        link: &LinkType,
+        direction: Direction,
+        location: PortLocation,
+    ) -> Result<(u32, u32)> {
+        // Ok, simple enough, pull out the relevant type, and get the port at location
+        match link {
+            LinkType::Node(id) => {
+                let Some(node) = self.managed_node_get(*id) else {
+                    bail!("Unable to Locate Node");
+                };
+
+                let id = node.pw_id.unwrap();
+                let port = node.port_map[location].unwrap();
+
+                Ok((id, port))
+            }
+            LinkType::Filter(id) => {
+                let filter = self.managed_filter_get(*id).unwrap();
+
+                let id = filter.pw_id.unwrap();
+                let port = filter.port_map[direction][location];
+
+                Ok((id, port))
+            }
+            LinkType::UnmanagedNode(id, port_map) => {
+                let node = self
+                    .unmanaged_device_node_get(*id)
+                    .ok_or_else(|| anyhow!("Unmanaged Device Node not Found"))?;
+
+                let ports = &node.ports[direction];
+
+                // Check whether the caller has explicitly told us which port to use
+                if let Some(link_ports) = port_map {
+                    let find = match location {
+                        PortLocation::Left => &link_ports.left,
+                        PortLocation::Right => &link_ports.right,
+                    };
+
+                    for (index, port) in ports.iter() {
+                        if port.channel == *find {
+                            return Ok((*id, *index));
+                        }
+                    }
+
+                    bail!("Unable to find channel");
+                }
+
+                // Check whether this is a mono device
+                if ports.iter().count() == 1
+                    && let Some(index) = ports.keys().next()
+                {
+                    return Ok((*id, *index));
+                }
+
+                // Iterate over the ports, try and find the location
+                for (index, port) in ports.iter() {
+                    if let Ok(port_location) = PortLocation::from_str(&port.channel)
+                        && port_location == location
+                    {
+                        return Ok((*id, *index));
+                    }
+                }
+
+                // If we get here, we didn't find anything, this shouldn't happen!
+                bail!("Requested Unmanaged Node is Neither Stereo or Mono");
+            }
+        }
     }
 
     pub fn managed_link_add(&mut self, id: Ulid, group: ManagedLink) {
@@ -210,4 +410,88 @@ impl Store {
             let _ = sender.unwrap().send(());
         }
     }
+}
+
+pub(crate) fn create_port_link(
+    core: &CoreRc,
+    parent_id: Ulid,
+    map: &mut ManagedLinkMap,
+    listener_store: Weak<RefCell<Store>>,
+) -> Result<()> {
+    let id = map.internal_id;
+    let (src_node, src_port) = map.source_port;
+    let (dest_node, dest_port) = map.destination_port;
+
+    let listener_info_store = listener_store;
+    let link = core
+        .create_object::<Link>(
+            "link-factory",
+            &properties! {
+                *LINK_OUTPUT_NODE => src_node.to_string(),
+                *LINK_OUTPUT_PORT => src_port.to_string(),
+                *LINK_INPUT_NODE => dest_node.to_string(),
+                *LINK_INPUT_PORT => dest_port.to_string(),
+                *OBJECT_LINGER => "false",
+                *NODE_PASSIVE => "false",
+            },
+        )
+        .map_err(|e| anyhow!("Failed to create link: {}", e))?;
+
+    let listener_bound_store = listener_info_store.clone();
+    let listener_error_store = listener_info_store.clone();
+    let proxy_listener = link
+        .upcast_ref()
+        .add_listener_local()
+        .bound(move |pw_id| {
+            // The link is now bound and has an ID, notify the store
+            if let Some(store) = listener_bound_store.upgrade() {
+                store.borrow_mut().managed_link_bound(parent_id, id, pw_id);
+            }
+        })
+        .error(move |seq, res, message| {
+            log::error!(
+                "[Link {}:{}] Link proxy error! seq={}, res={}, message={}",
+                parent_id,
+                id,
+                seq,
+                res,
+                message
+            );
+            // Notify the store about the error so the sender doesn't hang
+            if let Some(store) = listener_error_store.upgrade() {
+                store.borrow_mut().managed_link_error(parent_id, id);
+            }
+        })
+        .register();
+
+    let listener_done_store = listener_info_store.clone();
+    let listener_done_core = core.clone();
+    let state_done = Cell::new(false);
+    let link_listener = link
+        .add_listener_local()
+        .info(move |info| {
+            if info.change_mask().contains(LinkChangeMask::STATE) {
+                if state_done.get() {
+                    return;
+                }
+                if matches!(info.state(), LinkState::Active | LinkState::Paused) {
+                    state_done.set(true);
+
+                    if let Some(store) = listener_done_store.upgrade() {
+                        let seq = listener_done_core.sync(0).expect("core sync failed");
+                        store
+                            .borrow_mut()
+                            .set_pending_link_done(parent_id, id, seq.raw());
+                    }
+                }
+            }
+            //if matches!(info.state(), LinkState::Error(e) | LinkState::Unlinked) {}
+        })
+        .register();
+
+    map._link = Some(link);
+    map._proxy_listener = Some(proxy_listener);
+    map._info_listener = Some(link_listener);
+
+    Ok(())
 }
